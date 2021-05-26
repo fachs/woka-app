f6f0e96aec3390287aa5f1e5ce1e85ee7276a525
M Fachri <mfachri@upi.edu>
2021-05-26 10:30:14 +0700

    Add files via upload

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,14 +9,17 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Worker;
 use App\Models\Order;
+use App\Models\Service;
 
 class PagesController extends Controller
 {
     public function profil() {
-        $profil = Worker::findOrFail(1);
+        // $profil = Worker::findOrFail(1);
         $profil = Worker::where('user_id', Auth::user()->id)->firstOrFail();
-        
-        return view('profil.index',compact('profil'));
+        $dataOrder = Order::where('customer_id', Auth::user()->id)->get();
+        // $dataService = Service::where('id', $dataOrder->service_id)->get();
+        // , 'service' => $dataService
+        return view('profil.index',['profil' => $profil, 'order' => $dataOrder]);
     }
 
     public function profilWorker() {
@@ -31,7 +34,19 @@ class PagesController extends Controller
         
         // $pesanan = Orders::findOrFail(1);
         // $pesanan = Orders::where('customer_id', Auth::user()->id)->firstOrFail();
-        
-        return view('pesanan');
+        $ord_menunggu = Order::where('worker_id', Auth::user()->id)->where('status','menunggu')->get();
+        $ord_proses = Order::where('worker_id', Auth::user()->id)->where('status','diproses')->get();
+        $ord_ditolak = Order::where('worker_id', Auth::user()->id)->where('status','ditolak')->get();
+        $ord_selesai = Order::where('worker_id', Auth::user()->id)->where('status','selesai')->get();
+
+        return view('pesanan',['ord_menunggu' => $ord_menunggu,'ord_proses' => $ord_proses,'ord_ditolak' => $ord_ditolak,'ord_selesai' => $ord_selesai]);
+    }
+
+    public function search()
+    {
+        $search = Service::get();
+
+        return view('search.index',['search' => $search]);
     }
 }
+
