b36d0bc683b662020737aa33afcf540fdc5309a8
M Fachri <mfachri@upi.edu>
2021-05-31 12:41:57 +0700

    Add files via upload

--- app/Http/Controllers/WorkersController.php
+++ app/Http/Controllers/WorkersController.php
@@ -21,6 +21,16 @@ class WorkersController extends Controller
         // $workers = Worker::all();
     }
 
+     /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function done()
+    {
+        return view('gabung.done');
+    }
+
     /**
      * Show the form for creating a new resource.
      *
